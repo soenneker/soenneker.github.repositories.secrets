e1dc904aaf76df3b7ce75e54cce20163d871f6a2
Jake Soenneker <jake@soeneker.com>
2024-12-18 19:34:55 +0000

    xUnit v3

--- test/Soenneker.GitHub.Repositories.Secrets.Tests/GitHubRepositoriesSecretsUtilTests.cs
+++ test/Soenneker.GitHub.Repositories.Secrets.Tests/GitHubRepositoriesSecretsUtilTests.cs
@@ -1,7 +1,7 @@
 using Soenneker.GitHub.Repositories.Secrets.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
-using Xunit.Abstractions;
+
 
 namespace Soenneker.GitHub.Repositories.Secrets.Tests;
 
